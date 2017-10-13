9d6e80753727646e5319af4b40a00c0efbba432d
Jehan-HENRY <jehan.henry@gmail.com>
2017-10-13 11:45:11 +0200

    fixed bugs after merge

--- janeproject/src/components/ListPokemon.jsx
+++ janeproject/src/components/ListPokemon.jsx
@@ -5,16 +5,13 @@ import { browserHistory } from "react-router";
 
 const urlForPokemon = () => `https://pokeapi.co/api/v2/pokemon/?limit=811`;
 
-const square = {
-  width: 175,
-  height: 175
-};
+const square = { width: 175, height: 175 };
 
 class ListPokemon extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      search: "",
+      search: ""
     };
     this.capitalizeFirstLetter = this.capitalizeFirstLetter.bind(this);
     this.generateImage = this.generateImage.bind(this);
@@ -51,7 +48,11 @@ class ListPokemon extends Component {
   }
 
   generateImage(id) {
-    return ("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + id + ".png");
+    return (
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
+      id +
+      ".png"
+    );
   }
 
   updateInput(event) {
@@ -66,12 +67,15 @@ class ListPokemon extends Component {
   }
 
   updateResult() {
-    let newRes = this.state.pokemonData.results.filter(this.pokemonFilter)
     this.setState({
-      filtered:{
-        results: newRes
-      } 
-    })
+      pokemonData: this.state.pokemonData.results.filter(this.pokemonFilter)
+    });
+  }
+
+  pokemonFilter(pokemon) {
+    return (
+      pokemon.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
+    );
   }
 
   render() {
@@ -143,9 +147,5 @@ class ListPokemon extends Component {
     );
   }
 }
-        </div>
-      );
-    }
-  }
 
-  export default ListPokemon;
+export default ListPokemon;
